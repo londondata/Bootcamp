59a1adc51cc4cc3893a86afbfe03032d8c5a8d95
juney3 <juney37@gmail.com>
2018-06-19 09:11:48 -0700

    preparing for heroku deployment

--- bootcamp_project/bootcamp_project/settings.py
+++ bootcamp_project/bootcamp_project/settings.py
@@ -42,6 +42,10 @@ INSTALLED_APPS = [
     'rest_framework',
 ]
 
+MIDDLEWARE_CLASSES = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    ]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -113,16 +117,18 @@ REST_FRAMEWORK = {
         'rest_framework.parsers.FormParser',
     ],
 }
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL  = '/static/'
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, 'static'),
-]
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# Extra places for collectstatic to find static files.
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
