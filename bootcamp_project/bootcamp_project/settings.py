d63ede111073ba93a73e7b57bce0bf26bc1d2d3a
teripanda <teri.london@gmail.com>
2018-06-18 19:47:52 -0700

    preparing for deployment

--- bootcamp_project/bootcamp_project/settings.py
+++ bootcamp_project/bootcamp_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'dhne-_f$iw3*gu%dif6-e7v$^19c9s^ln2k!if2vzm1#px#=qs'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp']
 
 
 # Application definition
@@ -147,3 +148,4 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny'
     ]
 }
+ django_heroku.settings(locals())
